84ffb82c4d2ff2df558232a2d1193de533978d26
Mohammed Hattab <mohammedhattab97@gmail.com>
2023-08-27 20:59:42 -0400

    added swagger support

--- src/main/java/com/mtattab/emailservice/EmailServiceApplication.java
+++ src/main/java/com/mtattab/emailservice/EmailServiceApplication.java
@@ -1,9 +1,14 @@
 package com.mtattab.emailservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
+@OpenAPIDefinition
+//@EnableSwagger2WebMvc  // Enable Swagger with SpringDoc-OpenAPI
 public class EmailServiceApplication {
 
 	public static void main(String[] args) {
