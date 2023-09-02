cadef68000ca85b0101673a0adf1437a9d0a3b42
Mohammed Hattab <mohammedhattab97@gmail.com>
2023-09-02 13:43:13 -0400

    added cors config

--- src/main/java/com/mtattab/emailservice/config/ProjectSecurityConfig.java
+++ src/main/java/com/mtattab/emailservice/config/ProjectSecurityConfig.java
@@ -5,13 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.Collections;
 
-import static com.mtattab.emailservice.consts.Constants.WEB_DOMAIN;
 
 @Configuration
 public class ProjectSecurityConfig  {
@@ -30,20 +25,6 @@ public class ProjectSecurityConfig  {
 
         .anyRequest().authenticated().and().oauth2Login()
                 .and().logout().logoutSuccessUrl("/")
-                .and()
-                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList(WEB_DOMAIN));
-                        config.setAllowedMethods(Collections.singletonList("*"));
-                        config.setAllowCredentials(true);
-                        config.setAllowedHeaders(Collections.singletonList("*"));
-                        config.setExposedHeaders(Arrays.asList("Authorization"));
-                        config.setMaxAge(3600L);
-                        return config;
-                    }
-                }))
 
 
 ;
