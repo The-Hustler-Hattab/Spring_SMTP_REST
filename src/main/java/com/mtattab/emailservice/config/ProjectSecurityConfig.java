c113d514220ed99cd1527fab45235df5340a944c
Mohammed Hattab <mohammedhattab97@gmail.com>
2023-09-02 19:57:12 -0400

    added cors config

--- src/main/java/com/mtattab/emailservice/config/ProjectSecurityConfig.java
+++ src/main/java/com/mtattab/emailservice/config/ProjectSecurityConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Collections;
+
+import static com.mtattab.emailservice.consts.Constants.WEB_DOMAIN;
 
 
 @Configuration
@@ -25,7 +30,21 @@ public class ProjectSecurityConfig  {
 
         .anyRequest().authenticated().and().oauth2Login()
                 .and().logout().logoutSuccessUrl("/")
-
+                .and().cors(
+                        corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                CorsConfiguration config = new CorsConfiguration();
+                                config.setAllowedOrigins(Collections.singletonList(WEB_DOMAIN));
+                                config.setAllowedMethods(Collections.singletonList("*"));
+                                config.setAllowCredentials(true);
+                                config.setAllowedHeaders(Collections.singletonList("*"));
+                                config.setMaxAge(3600L);
+                                return config;
+                            }
+                        }
+                    )
+                )
 
 ;
         return http.build();
--- src/main/java/com/mtattab/emailservice/consts/Constants.java
+++ src/main/java/com/mtattab/emailservice/consts/Constants.java
@@ -4,5 +4,6 @@ public interface Constants {
 
     public static final String JSON ="application/json";
 
+    public static final String WEB_DOMAIN="https://project-smtp.mtattab.com";
 
 }
